866dc9b4bf45689733501b7c655b98ac223ec864
Aidar Khabirov <khabirov.ar1990@gmail.com>
2024-09-12 08:09:25 +0500

    Try string builder

--- module01/03.reverse.go
+++ module01/03.reverse.go
@@ -1,14 +1,16 @@
 package module01
 
+import "strings"
+
 // Reverse will return the provided word in reverse
 // order. Eg:
 //
 //	Reverse("cat") => "tac"
 //	Reverse("alphabet") => "tebahpla"
 func Reverse(word string) string {
-	reversed := ""
-	for _, letter := range word {
-		reversed = string(letter) + reversed
+	var sb strings.Builder
+	for i := len(word) - 1; i >= 0; i-- {
+		sb.WriteByte(word[i])
 	}
-	return reversed
+	return sb.String()
 }
