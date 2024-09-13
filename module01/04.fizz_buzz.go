a3fe9b2d164e48661bbe4a50f007548bb956596e
Aidar Khabirov <khabirov.ar1990@gmail.com>
2024-09-13 08:18:30 +0500

    Solve fizzbuzz

--- module01/04.fizz_buzz.go
+++ module01/04.fizz_buzz.go
@@ -1,5 +1,10 @@
 package module01
 
+import (
+	"fmt"
+	"strings"
+)
+
 // FizzBuzz will print out all of the numbers
 // from 1 to N replacing any divisible by 3
 // with "Fizz", and divisible by 5 with "Buzz",
@@ -11,4 +16,20 @@ package module01
 // I wouldn't normally recommend this, but did
 // it here to make life easier for beginners.
 func FizzBuzz(n int) {
+	var sb strings.Builder
+	sb.WriteString("1")
+	for i := 2; i <= n; i++ {
+		sb.WriteString(", ")
+		switch {
+		case i%5 == 0 && i%3 == 0:
+			sb.WriteString("Fizz Buzz")
+		case i%3 == 0:
+			sb.WriteString("Fizz")
+		case i%5 == 0:
+			sb.WriteString("Buzz")
+		default:
+			sb.WriteString(fmt.Sprintf("%d", i))
+		}
+	}
+	fmt.Println(sb.String())
 }
