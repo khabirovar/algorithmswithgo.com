cb84594bf2b73bc0fbec68bf8e0586b66de63955
Aidar Khabirov <khabirov.ar1990@gmail.com>
2024-09-19 12:36:37 +0500

    Solve base_to_dec

---
+++ module01/06.base_to_dec.go
@@ -0,0 +1,32 @@
+package module01
+
+import (
+	"math"
+	"strconv"
+)
+
+// BaseToDec takes in a number and the base it is currently
+// in and returns the decimal equivalent as an integer.
+//
+// Eg:
+//
+//	BaseToDec("E", 16) => 14
+//	BaseToDec("1110", 2) => 14
+func BaseToDec(value string, base int) int {
+	dec := 0
+	value = Reverse(value)
+	for idx, num := range value {
+
+		dec += int(math.Pow(float64(base), float64(idx))) * getNumbered(num)
+	}
+	return dec
+}
+
+func getNumbered(r rune) int {
+	var num int
+	if r >= '0' && r <= '9' {
+		num, _ = strconv.Atoi(string(r))
+		return num
+	}
+	return int(r-'A') + 10
+}
